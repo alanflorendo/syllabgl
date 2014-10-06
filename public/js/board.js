0c4ebb461053a3a19e1394ba7931d9ae248a27d9
alanflorendo <alan.florendo@gmail.com>
2014-10-06 19:10:08 -0400

    make board 5X5 instead of 4X4

--- public/js/board.js
+++ public/js/board.js
@@ -1,12 +1,12 @@
 
 function Board() {
+	SIDE = 5
 	var dice = [];
 
 	this.generateDice = function() {
 	  var lg = new LetterGenerator;
-	  var dice = [];
 
-	  for (var i=0; i<16; i++){
+	  for (var i=0; i<(SIDE * SIDE); i++){
 	    newDie = new Die(lg.getRandomFace());
 	    dice.push(newDie);
 	  }
@@ -17,8 +17,8 @@ function Board() {
 	this.putBoardInDOM = function() {
 		this.dice = this.generateDice();
 		var i = 0;
-		for (var tr=1; tr<=4; tr++){ // go thru each row
-			for (var td=1; td<=4; td++){ // go thru each col
+		for (var tr=1; tr<=SIDE; tr++){ // go thru each row
+			for (var td=1; td<=SIDE; td++){ // go thru each col
 				var selector = '#board tr:nth-child(' + tr + ') td:nth-child(' + td +')'
 				$(selector).html(this.dice[i].face);
 				i += 1;
