31d2e054cd8f8a1bc736a177c53d7815369290f2
alanflorendo <alan.florendo@gmail.com>
2014-10-06 20:03:50 -0400

    add function findPossibleFirsts.  for a given word, it returns an array of tiles that could start the word.

--- public/js/wordChecker.js
+++ public/js/wordChecker.js
@@ -1,5 +1,28 @@
-function WordChecker() {
+function WordChecker(word, board) {
+	this.possibileFirsts = [];
+
+	this.wordExcerpt = function(word, start, len) {
+		var chars = []
+		for (var i=0; i<len; i++) {
+			chars.push(word[start+i]);
+		}	
+		return chars.join("");
+	}
+
+	this.findPossibleFirsts = function() {
+		var i = 0;
+		for (var tr=1; tr<=SIDE; tr++){ // go thru each row
+			for (var td=1; td<=SIDE; td++){ // go thru each col
+				var die = board.dice[i]
+				var textLength = die.face.length;
+				if (die.face === this.wordExcerpt(word, 0, textLength)) {
+					this.possibileFirsts.push(die);
+				}
+			i += 1;
+			}
+		}
+	}
 
 
-	
+
 }
