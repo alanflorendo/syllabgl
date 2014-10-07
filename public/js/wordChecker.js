d7872c1999a2854bf9225bc20db9294d43ef7aaa
alanflorendo <alan.florendo@gmail.com>
2014-10-07 00:16:33 -0400

    error checking, done for the night

--- public/js/wordChecker.js
+++ public/js/wordChecker.js
@@ -25,22 +25,34 @@ function WordChecker(word, board) {
 
 	this.traceOutPossiblesOneDie = function() {
 		var newPossibles = [];
+		console.log("Just set up newPossibles array as empty array: ");
+		console.log(newPossibles);
 		for (var i=0; i<this.possibleCombos.length; i++) {
 			var numLettersSoFar = 0;
 			// go thru each possible combo and determine the number of letter so far
 			for (var j=0; j<this.possibleCombos[i].length; j++) {
 				numLettersSoFar += this.possibleCombos[i][j].face.length;
+				console.log(numLettersSoFar + " came from adding this many letters: " + this.possibleCombos[i][j].face.length)
 			}
 			// then for each of the neighbors of the last die in the combo
-			var lastDie = this.possibleCombos[0][this.possibleCombos[0].length-1]
+			var lastDie = this.possibleCombos[i][this.possibleCombos[i].length-1]
+			console.log("JS line 36:  Last Die:");
+			console.log(lastDie.dom);
 			for (var n=0; n<lastDie.neighbors.length ; n++) {
+				console.log("neighbor # " + n + " is this face, " + lastDie.neighbors[n].face)
 				var neighbor = lastDie.neighbors[n];
 				if (neighbor.face === this.wordExcerpt(word, numLettersSoFar, neighbor.face.length)) {
+					console.log("JS Line 45:  so we've got a match")
 					var comboToPush = this.possibleCombos[i].push(neighbor);
 					newPossibles.push(comboToPush);
+					console.log("Here is the combo!");
+					console.log(comboToPush);
+					console.log(newPossibles);
 				}
 			}
 		}
+		console.log("at the end of the loop");
+		console.log(newPossibles);
 		this.possibles = newPossibles;
 	}
 
