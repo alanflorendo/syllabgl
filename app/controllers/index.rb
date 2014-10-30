4038e179e017ac5637a03372f271cc1def70f7fd
alanflorendo <alan.florendo@gmail.com>
2014-10-30 12:32:03 -0400

    add json routes for game (without solution) and separate solution

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,21 @@ get '/' do
   erb :index
 end
 
+get '/game.json' do
+	content_type :json
+	game = GameScenario.find(rand(GameScenario.count))
+	limited_game = { id: game.id, faces: game.faces, max_score: game.max_score }
+	limited_game.to_json
+end
+
+
+get '/game/solution/:id.json' do
+	content_type :json
+	game = GameScenario.find(params[:id])
+	solution = { viable_words: game.viable_words }
+	solution.to_json
+end
+
 get '/game_scenarios' do
 	@gss = GameScenario.all
 	erb :'game_scenarios/index'
